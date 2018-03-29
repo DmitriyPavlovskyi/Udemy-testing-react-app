973246ec04375a69d8b0221197e5fdc5d1679331
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-03-29 15:27:54 +0300

    Comment box ui logic covered with tests

--- src/features/commentBox/index.js
+++ src/features/commentBox/index.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class CommentBox extends Component {
-  static propTypes = {
+  constructor(props) {
+    super(props);
 
-  };
+    this.state = { comment: '' };
+  }
 
   render() {
     return (
-      <div className="comment-box">
-        <textarea />
-        <button>Submit comment</button>
-      </div>
+      <form onSubmit={this.handleSubmit} className="comment-box">
+        <textarea
+          value={this.state.comment}
+          onChange={this.handleChange}
+        />
+      <button action="submit">Submit comment</button>
+      </form>
     );
+  }
+
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({ comment: '' });
   }
 }
 
--- test/components/commentBox_test.js
+++ test/components/commentBox_test.js
@@ -4,6 +4,7 @@ import CommentBox from '../../src/features/commentBox/index';
 describe('Comment Box test', () => {
   let component;
 
+  // running beforeEach in nested describe as well
   beforeEach(() => {
     component = renderComponent(CommentBox);
   });
@@ -20,4 +21,19 @@ describe('Comment Box test', () => {
     expect(component.find('button')).to.exist;
   });
 
+  describe('Entering some text', () => {
+    // this beforeEach Not running in upper scope
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('Shows that text in the text area', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('When submited, clears the input', () => {
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
+    });
+  });
 });
