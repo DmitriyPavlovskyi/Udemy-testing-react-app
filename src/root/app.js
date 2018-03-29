d79172584359d38f91eea890e0ba4d0a7a532758
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-03-29 09:24:26 +0300

    First spec added

--- src/root/app.js
+++ src/root/app.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
   render() {
     return (
       <div>
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,11 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/root/app';
 
-describe('App' , () => {
-  let component;
+describe('App', () => {
+  it('Shows the correct text', () => {
+    // Create an instance of app
+    const component = renderComponent(App);
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
+    expect(component).to.contain('Hello World!')
   });
 });
